936beac2e989f9cdddb8327d840d9456806d5424
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 10:18:58 +0300

    Update Organization.cs
    Add validation

--- Database/Models/Organization.cs
+++ Database/Models/Organization.cs
@@ -5,14 +5,19 @@ namespace Database.Models
     public class Organization
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter organization name, please!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter organization code, please!")]
         public string Code { get; set; }
+        [Required(ErrorMessage = "Enter organization type, please!")]
         public string Type { get; set; }
+        [Required(ErrorMessage = "Choose one of the options")]
         public bool GeneralPartnership { get; set; }
         public bool LimitedPartnership { get; set; }
         public bool LimitedLiabilityCompany { get; set; }
         public bool IncorporatedCompany { get; set; }
         public bool Owner { get; set; }
+        [Required(ErrorMessage = "Enter any other information, please!")]
         public string Other { get; set; }
 
         public int EnterpriseId { get; set; }
