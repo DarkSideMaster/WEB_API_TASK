6debb4f0a5f3e3defdb5b45fedd21863cba7dbdc
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 10:25:28 +0300

    Update User.cs
    Add validation

--- Database/Models/User.cs
+++ Database/Models/User.cs
@@ -3,9 +3,13 @@
     public class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter first name, please!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter second name, please!")]
         public string Surname { get; set; }
+        [EmailAddress(ErrorMessage = "Please enter a valid Email address")] 
         public string Email { get; set; }
+        [Required(ErrorMessage = "Enter your adress, please!")]
         public string Adress { get; set; }
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
