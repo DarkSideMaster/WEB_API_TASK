c8e115579928c90b59f07e0a232f469fec649bd6
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 10:04:21 +0300

    Update Enterprise.cs
    Add validation

--- Database/Models/Enterprise.cs
+++ Database/Models/Enterprise.cs
@@ -5,6 +5,7 @@ namespace Database.Models
     public class Enterprise
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter Enterprise name, please!")]
         public string Name { get; set; }
 
         public List<Organization> Organizations { set; get; }
