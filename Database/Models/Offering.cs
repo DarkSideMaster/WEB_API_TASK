b75ba85d9db455124852f392b755e000dd74365b
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 10:05:41 +0300

    Update Offering.cs
    Add validation

--- Database/Models/Offering.cs
+++ Database/Models/Offering.cs
@@ -5,8 +5,8 @@ namespace Database.Models
     public class Offering
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter offering name, please!")]
         public string Name { get; set; }
-
         public int FamilyId { get; set; }
         public Family Family { get; set; }
         public List<Department> Departments { set; get; }
