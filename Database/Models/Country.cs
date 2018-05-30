d17b8c748e55109d7c931a79b309850fbe9f8695
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 10:03:00 +0300

    Update Country.cs
    Add validation

--- Database/Models/Country.cs
+++ Database/Models/Country.cs
@@ -5,8 +5,10 @@ namespace Database.Models
 
     public class Country
     {
-        public int Id { get; set; }        
+        public int Id { get; set; }  
+        [Required(ErrorMessage = "Enter country name, please!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter country code, please!")]
         public int Code { get; set; }
         public int OrganizationId { get; set; }
         public Organization Organization { get; set; }
