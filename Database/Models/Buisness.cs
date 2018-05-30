efe24a9ef605d26eb9a5da900f3325a882f04897
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 09:59:45 +0300

    Update Buisness.cs
    Add validation

--- Database/Models/Buisness.cs
+++ Database/Models/Buisness.cs
@@ -5,6 +5,7 @@ namespace Database.Models
     public class Business
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter a name of your Business, please!")]
         public string Name { get; set; }
         public int CountryId { get; set; }
         public Country Country { get; set; }
