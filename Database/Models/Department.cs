244c5526e883374a4738ba8319b6fc38c159a5c1
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 10:03:32 +0300

    Update Department.cs
    Add validation

--- Database/Models/Department.cs
+++ Database/Models/Department.cs
@@ -5,6 +5,7 @@ namespace Database.Models
     public class Department
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter department name, please!")]
         public string Name { get; set; }
         public int OfferingId { get; set; }
         public Offering Offering { get; set; }
