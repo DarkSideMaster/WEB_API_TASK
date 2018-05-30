5ed5d1a72b4c4e6c4067bcb82c181db37c62c5bb
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-30 10:05:03 +0300

    Update Family.cs
    Add validation

--- Database/Models/Family.cs
+++ Database/Models/Family.cs
@@ -5,6 +5,7 @@ namespace Database.Models
     public class Family
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter family name, please!")]
         public string Name { get; set; }
         public int BuisnessId { get; set; }
         public Business Business { get; set; }
