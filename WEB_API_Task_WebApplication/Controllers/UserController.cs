136ace73eab96b420d786ab649bdd9044efeaff6
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-05-31 09:57:10 +0300

    Update UserController.cs

--- WEB_API_Task_WebApplication/Controllers/UserController.cs
+++ WEB_API_Task_WebApplication/Controllers/UserController.cs
@@ -23,14 +23,14 @@ namespace WEB_API_Task_WebApplication.Controllers
         
         [AllowAnonymous]
         [HttpGet]
-        public List<User> GetUsers()
+        public IActionResult GetUsers()
         {
-            return _userRepository.Entities;
+            return Ok(_userRepository.Entities);
         }
 
         [Route("Create")]
         [HttpPost]
-        public List<User> Create(User item)
+        public IActionResult Create(User item)
         {
             if (item == null)
             {
@@ -45,12 +45,12 @@ namespace WEB_API_Task_WebApplication.Controllers
 
             _userRepository.Create(item);
 
-            return _userRepository.Entities;
+            return Ok(_userRepository.Entities);
         }
 
         [Route("Delete")]
         [HttpPost]
-        public List<User> Delete(int Id)
+        public IActionResult Delete(int Id)
         {
             if (Id < 0)
             {
@@ -65,12 +65,12 @@ namespace WEB_API_Task_WebApplication.Controllers
 
             _userRepository.Delete(Id);
 
-            return _userRepository.Entities;
+            return Ok(_userRepository.Entities);
         }
         
         [Route("Update")]
         [HttpPost]
-        public List<User> Update(User item) 
+        public IActionResult Update(User item) 
         {
 
             if (item == null)
@@ -86,7 +86,7 @@ namespace WEB_API_Task_WebApplication.Controllers
 
             _userRepository.Update(item);
 
-            return _userRepository.Entities;
+            return Ok(_userRepository.Entities);
         }
     }
 }
