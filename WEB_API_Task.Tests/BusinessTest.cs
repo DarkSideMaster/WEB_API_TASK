3c1a4d9934aeae6bdc1c3b64d02de7a3de4ce149
Andrii <darkpanter@ukr.net>
2018-06-03 22:08:49 +0300

    Update unit test

--- WEB_API_Task.Tests/BusinessTest.cs
+++ WEB_API_Task.Tests/BusinessTest.cs
@@ -17,11 +17,11 @@ namespace WEB_API_Task.Tests
             // var controller = new BusinessController(new EnterpriseContext(new DbContextOptions<EnterpriseContext>()));  
             //   var result = controller.GetBuisnesses();     
 
-            //var responseString = response.Content.ReadAsString().Result;
+            var responseString = response.Content.ReadAsString().Result;
 
-            //dynamic jsonObject = JObject.Parse(responseString);
-            //int Id = (int)jsonObject.Id;
-            //Assert.Equal(1, Id);
+            dynamic jsonObject = JObject.Parse(responseString);
+            int Id = (int)jsonObject.Id;
+            Assert.Equal(1, Id);
 
         }
     }
