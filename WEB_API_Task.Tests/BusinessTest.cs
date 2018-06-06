860d3a782cc614a41d0e2d5bd175b9280caa39ae
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-06-06 10:24:51 +0300

    Update BusinessTest.cs

--- WEB_API_Task.Tests/BusinessTest.cs
+++ WEB_API_Task.Tests/BusinessTest.cs
@@ -38,7 +38,7 @@ namespace WEB_API_Task.Tests
               Assert.IsNotNull(valueresult);
               Assert.IsNotNull(okObjectResult);
               Assert.IsType<Business>(okObjectResult);
-           //  Assert.Equal(expectedBusinessCount, okObjectResult);           
+           // Assert.Equal(expectedBusinessCount, okObjectResult);           
           // Assert.AreEqual(presentations.Select(g => g.Id).Intersect(gs1Data.Select(d => d.Id)).Count(),
           //  presentations.Count());
                     
