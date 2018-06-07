d86e508392f61c39e0571a5220504560a796f641
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-06-07 09:41:40 +0300

    Update EnterpriseController.cs

--- WEB_API_Task_WebApplication/Controllers/EnterpriseController.cs
+++ WEB_API_Task_WebApplication/Controllers/EnterpriseController.cs
@@ -93,5 +93,30 @@ namespace WEB_API_Task.Controllers
                 return BadRequest(ModelState); //or you can throw new Exception
             }
         }
+        
+        [Route("Filter")]
+        [HttpPost]
+        public IActionResult Filter(string name)
+        {              
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {  
+            var enterprise = new Enterprise();
+            
+            if (enterprise.Name != name)
+            {
+                return Content("Error");
+            }         
+                return Ok(_enterpriseRepository.FilterEnterprise(name));
+            }
+            catch
+            {
+                return BadRequest(ModelState); //or can throw new Exception
+            }   
+        }                 
     }
 }
