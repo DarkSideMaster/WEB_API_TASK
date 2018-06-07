df37e8ff9c51fa7a9b07de2ac1607e1a9365d25f
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-06-07 09:36:39 +0300

    Update DepartmentController.cs

--- WEB_API_Task_WebApplication/Controllers/DepartmentController.cs
+++ WEB_API_Task_WebApplication/Controllers/DepartmentController.cs
@@ -87,5 +87,30 @@ namespace WEB_API_Task.Controllers
                 return BadRequest(ModelState); //or can throw new Exception
             }
         }
+        
+        [Route("Filter")]
+        [HttpPost]
+        public IActionResult Filter(string name)
+        {              
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {  
+            var department = new Department();
+            
+            if (department.Name != name)
+            {
+                return Content("Error");
+            }         
+                return Ok(_departmentRepository.FilterDepartment(name));
+            }
+            catch
+            {
+                return BadRequest(ModelState); //or can throw new Exception
+            }   
+        }         
     }
 }
